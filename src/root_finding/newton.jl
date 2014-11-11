3fa73c69f64c47d0af9606cf2e16bfdfad48e1c1
Luis Benet <benet@fis.unam.mx>
2014-11-11 11:43:39 -0600

    Changes in newton.jl
    I have cleaned-up the code, changed the way of calling `N` (now
    without any reference to f_prime), and changed the symbols associated
    with the roots. I included an exit criterion related with the number
    of levels that can be tried (for now, being 30).

--- src/root_finding/newton.jl
+++ src/root_finding/newton.jl
@@ -4,38 +4,29 @@ function guarded_mid(x::Interval)
     m = mid(x)
     if m == zero(x.lo)  # midpoint exactly 0
         alpha = 0.45
-        m = alpha*x.lo + (1-alpha)*x.hi   # displace to another point in the interval
+        m = alpha*x.lo + (1.0-alpha)*x.hi   # displace to another point in the interval
     end
 
     m
 end
 
-function N(f::Function, f_prime::Function, x::Interval, deriv=None)
-
-    if deriv==None
-        deriv = f_prime(x)
-    end
-
+function N(f::Function, x::Interval, deriv::Interval)
     m = guarded_mid(x)
     m = Interval(m)
-
-    Nx = m - ( f(m) / deriv )
-
+    Nx = m - f(m) * inv(deriv)
+    Nx
 end
 
-
-
-function newton_refine(f::Function, f_prime::Function, x::Interval, tolerance=1e-16)
+function newton_refine(f::Function, f_prime::Function, x::Interval, tolerance=1e-15)
     #print("Entering newton_refine:")
     #@show x
 
     while diam(x) > tolerance  # avoid problem with tiny floating-point numbers if 0 is a root
-        Nx = N(f, f_prime, x)
-        Nx = Nx ∩ x
 
-        if Nx == x
-            return Any[(x, :unique)]
-        end
+        deriv = f_prime(x)
+        Nx = N(f, x, deriv)
+        Nx = Nx ∩ x
+        Nx == x && break
         x = Nx
 
     end
@@ -46,92 +37,83 @@ end
 #newton(f::Function, x::Nothing) = []
 
 
-newton(f::Function, x::Interval, tolerance=1e-16) = newton(f, D(f), x, 0, tolerance)
+newton(f::Function, x::Interval, tolerance=1e-15) = newton(f, D(f), x, 0, tolerance)
  # use automatic differentiation if no derivative function given
 
-function newton(f::Function, f_prime::Function, x::Interval, level::Int=0, tolerance=1e-16)
-
-    if isempty(x)
-#        if level==0
-#            return [(∅, :none)]
- #       else
-            return Any[]
-  #      end
-    end
-
+function newton(f::Function, f_prime::Function, x::Interval, level::Int=0, tolerance=1e-15)
+    
     #print("Entering Newton: ")
     #@show(x, level)
 
-    if diam(x) < tolerance
-        return Any[(x, :unknown)]
-    end
+    level >= 30 && return Any[(x, :unknown)]
 
+    isempty(x) && return Any[(x, :empty)]
+
+    # Shall we make sure tolerance>eps(1.0) ?
+    z = zero(x.lo)
+    tolerance = abs(tolerance)
+    if diam(x) < tolerance
+        z in f(x) && newton(f, f_prime, x, level+1)
+        println("Error: ", z in f(x), " ", x, " ", f(x))
+        return Any[(x, :error)]
+    end
 
     #deriv = differentiate(f, x)
     deriv = f_prime(x)
 
-    if !(0 in deriv)
-        Nx = N(f, f_prime, x, deriv)
+    if !(z in deriv)
 
-        if isempty(Nx ∩ x)
-         #   if level==0
-         #       return [(∅, :none)]
-         #   else
-                return Any[]
-          #  end
-        end
+        # Nx = N(f, f_prime, x, deriv)
+        Nx = N(f, x, deriv)
 
-        if Nx ⊆ x
-            return newton_refine(f, f_prime, Nx)
-        end
+        isempty(Nx ∩ x) && return Any[(x, :none)]
 
+        Nx ⊆ x && return newton_refine(f, f_prime, Nx)
 
-        if isthin(x)
-            return Any[(x, :unknown)]
-        end
+        # if isthin(x)
+        #     println(0 in f(x))
+        #     return Any[(x, :unknown)]
+        # end
 
         # bisect:
 
         #println("Bisecting...")
 
-        m = mid(x)
+        m = guarded_mid(x)
 
-        roots = vcat(newton(f, f_prime, Interval(x.lo, m), level+1),  # must be careful with rounding of m ?
+        rootsN = vcat(
+                    newton(f, f_prime, Interval(x.lo, m), level+1),  # must be careful with rounding of m ?
                     newton(f, f_prime, Interval(m, x.hi), level+1)
                     )
 
-        # if length(roots) == 0 && level==0
-        #    return [(∅, :none)]
-        #else
-            return sort!(roots)
-        #end
-
     else  # 0 in deriv; this does extended interval division by hand
-        y1 = Interval(deriv.lo, -zero(deriv.lo))
-        y2 = Interval(zero(deriv.lo), deriv.hi)
 
-        y1 = N(f, f_prime, x, y1) ∩ x
-        y2 = N(f, f_prime, x, y2) ∩ x
+        y1 = Interval(deriv.lo, -z)
+        y2 = Interval(z, deriv.hi)
 
-        roots = vcat(
-                         newton(f, f_prime, y1, level+1),
-                         newton(f, f_prime, y2, level+1)
-                         )
+        # y1 = N(f, f_prime, x, y1) ∩ x
+        # y2 = N(f, f_prime, x, y2) ∩ x
+        y1 = N(f, x, y1) ∩ x
+        y2 = N(f, x, y2) ∩ x
 
-        #@show roots
+        rootsN = vcat(
+                    newton(f, f_prime, y1, level+1),
+                    newton(f, f_prime, y2, level+1)
+                    )
 
-      #  if length(roots) == 0 && level==0
-      #      return [(∅, :none)]
-      #  else
-            return sort!(roots)
-       # end
-
-#         return sort!(vcat(
-#                          newton(f, f_prime, y1),
-#                          newton(f, f_prime, y2)
-#                          )
-#                      )
     end
+
+    # This cleans-up the tuples with `:none` or `:empty` from the rootsN vector
+    rrootsN = Any[]
+    for i in 1:length(rootsN)
+        tup = copy(rootsN[i])
+        (tup[2] == symbol(:none) || tup[2] == symbol(:empty)) && continue
+         push!(rrootsN, tup)
+    end
+
+    # return sort!(rootsN)
+    return sort!(rrootsN)
+
 end
 
 # function process_newton(f::Function, x::Interval)
