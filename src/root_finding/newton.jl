fb74c5178155b4e8fda07f16d10cd9a292031a6c
Luis Benet <benet@fis.unam.mx>
2014-11-25 20:02:51 -0600

    Add "tolerance" and "debug" as keyword arguments to `newton`
    Added "tolerance" and "debug" as keyword arguments to newton,
    following the implementation of David (branch improve_root_finding).
    They have as default values `eps(one(T))` (with T the type of
    the elements of the interval) and `false`.

--- src/root_finding/newton.jl
+++ src/root_finding/newton.jl
@@ -17,36 +17,31 @@ function N(f::Function, x::Interval, deriv::Interval)
     Nx
 end
 
-function newton_refine{T<:Real}(f::Function, f_prime::Function, x::Interval{T}, 
-                                tolerance=eps(one(T)))
-    #print("Entering newton_refine:")
-    #@show x
+function newton_refine{T<:Real}(f::Function, f_prime::Function, x::Interval{T}; 
+    tolerance=eps(one(T)), debug=false)
+
+    debug && (print("Entering newton_refine:"); @show x)
 
     while diam(x) > tolerance  # avoid problem with tiny floating-point numbers if 0 is a root
-
         deriv = f_prime(x)
         Nx = N(f, x, deriv)
+        debug && @show(x, Nx)
         Nx = Nx ∩ x
         Nx == x && break
         x = Nx
-
     end
 
     Any[(x, :unique)]
 end
 
-#newton(f::Function, x::Nothing) = []
+# use automatic differentiation if no derivative function given
+newton{T<:Real}(f::Function, x::Interval{T}; tolerance=eps(one(T)), debug=false) = 
+    newton(f, D(f), x, 0, tolerance=tolerance, debug=debug)
 
+function newton{T<:Real}(f::Function, f_prime::Function, x::Interval{T}, level::Int=0; 
+    tolerance=eps(one(T)), debug=false)
 
-newton{T<:Real}(f::Function, x::Interval{T}, tolerance=eps(one(T))) = 
-                newton(f, D(f), x, 0, tolerance)
- # use automatic differentiation if no derivative function given
-
-function newton{T<:Real}(f::Function, f_prime::Function, x::Interval{T}, level::Int=0, 
-                        tolerance=eps(one(T)))
-    
-    #print("Entering Newton: ")
-    #@show(x, level)
+    debug && (print("Entering newton:"); @show (x, level))
 
     level >= 30 && return Any[(x, :unknown)]
 
@@ -56,7 +51,7 @@ function newton{T<:Real}(f::Function, f_prime::Function, x::Interval{T}, level::
     z = zero(x.lo)
     tolerance = abs(tolerance)
     if diam(x) < tolerance
-        z in f(x) && newton(f, f_prime, x, level+1)
+        z in f(x) && newton(f, f_prime, x, level+1, tolerance=tolerance, debug=debug)
         println("Error: ", z in f(x), " ", x, " ", f(x))
         return Any[(x, :error)]
     end
@@ -71,22 +66,16 @@ function newton{T<:Real}(f::Function, f_prime::Function, x::Interval{T}, level::
 
         isempty(Nx ∩ x) && return Any[(x, :none)]
 
-        Nx ⊆ x && return newton_refine(f, f_prime, Nx)
+        Nx ⊆ x &&  (debug && (print("Refining "); @show(x)); 
+            return newton_refine(f, f_prime, Nx, tolerance=tolerance, debug=debug))
 
-        # if isthin(x)
-        #     println(0 in f(x))
-        #     return Any[(x, :unknown)]
-        # end
+        m = guarded_mid(x) # must be careful with rounding of m ?
 
-        # bisect:
-
-        #println("Bisecting...")
-
-        m = guarded_mid(x)
+        debug && @show (x, m)
 
         rootsN = vcat(
-                    newton(f, f_prime, Interval(x.lo, m), level+1),  # must be careful with rounding of m ?
-                    newton(f, f_prime, Interval(m, x.hi), level+1)
+                    newton(f, f_prime, Interval(x.lo, m), level+1, tolerance=tolerance, debug=debug),
+                    newton(f, f_prime, Interval(m, x.hi), level+1, tolerance=tolerance, debug=debug)
                     )
 
     else  # 0 in deriv; this does extended interval division by hand
@@ -94,15 +83,27 @@ function newton{T<:Real}(f::Function, f_prime::Function, x::Interval{T}, level::
         y1 = Interval(deriv.lo, -z)
         y2 = Interval(z, deriv.hi)
 
+        if debug
+            println("0 in deriv")
+            @show deriv
+            @show (y1, y2)
+            @show N(f, x, y1)
+            @show N(f, x, y2)
+        end
+
         # y1 = N(f, f_prime, x, y1) ∩ x
         # y2 = N(f, f_prime, x, y2) ∩ x
         y1 = N(f, x, y1) ∩ x
         y2 = N(f, x, y2) ∩ x
 
+        debug && @show(y1, y2)
+
         rootsN = vcat(
-                    newton(f, f_prime, y1, level+1),
-                    newton(f, f_prime, y2, level+1)
+                    newton(f, f_prime, y1, level+1, tolerance=tolerance, debug=debug),
+                    newton(f, f_prime, y2, level+1, tolerance=tolerance, debug=debug)
                     )
+
+        debug && show(rootsN)
 
     end
 
@@ -114,31 +115,6 @@ function newton{T<:Real}(f::Function, f_prime::Function, x::Interval{T}, level::
          push!(rrootsN, tup)
     end
 
-    # return sort!(rootsN)
     return sort!(rrootsN)
 end
 
-# function process_newton(f::Function, x::Interval)
-
-#     roots = newton(f, x)
-
-#     unique_roots = Interval[]
-#     unknown_roots = Interval[]
-
-#     for root in roots
-#         @show root
-#         if root[2] == :unique
-#             push!(unique_roots, root[1])
-#         else
-#             push!(unknown_roots, root[1])
-#         end
-#     end
-
-#     sort!(unique_roots)
-#     sort!(unknown_roots)
-
-#     unique_roots, unknown_roots
-# end
-
-#import Base.show
-#show(io::IO, x::Interval) = print(io, "[$(round(float(x.lo), 5)), $(round(float(x.hi), 5))]")
