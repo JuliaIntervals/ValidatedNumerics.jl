2f55e2bc3ce7ba6c825baaf59d37fac2fe3c454e
David P. Sanders <dpsanders@gmail.com>
2015-05-12 14:14:11 -0500

    Fleshed out the interface

--- src/intervals/interval_parameters.jl
+++ src/intervals/interval_parameters.jl
@@ -3,13 +3,14 @@ using ValidatedNumerics
 type IntervalParameters
 
     precision_type::Type
-    bigfloat_precision::Int
+    precision::Int
 
     rounding_mode::RoundingMode
 
-    big_pi::Interval{BigFloat}
-    float_pi::Interval{Float64}
+    pi::Interval{BigFloat}
 end
+
+const float_pi = make_interval(Float64, pi)  # does not change
 
 # default values:
 IntervalParameters() =
@@ -18,7 +19,6 @@ IntervalParameters() =
         256,
         RoundNearest,
         @interval(pi),
-        @floatinterval(pi)
     )
 
 
@@ -29,8 +29,20 @@ function set_interval_precision(T::Type, prec::Int=256)
     interval_parameters.precision_type = T
 
     if T==BigFloat
-        interval_parameters.bigfloat_precision = prec
+        interval_parameters.precision = prec
         set_bigfloat_precision(prec)
-        interval_constants.big_pi = @interval(pi)
+        interval_parameters.big_pi = @interval(pi)
     end
 end
+
+get_interval_precision() =
+    interval_parameters.precision_type == Float64 ? (Float64, -1)
+                                                  : (BigFloat, interval_parameters.precision)
+
+
+set_interval_precision(prec) = set_interval_precision(BigFloat, prec)
+
+get_pi(::Type{BigFloat}) = interval_parameters.pi
+get_pi(::Type{Float64}) = float_pi
+
+get_pi() = get_pi(interval_parameters.precision_type)
