cb6111ed0544947a3d2b50fe0a8b9305e9732430
David P. Sanders <dpsanders@gmail.com>
2015-05-12 19:16:47 -0500

    @interval now creates intervals according to the parameters in interval_parameters

--- src/intervals/intervals.jl
+++ src/intervals/intervals.jl
@@ -1,11 +1,16 @@
 include("interval_definition.jl")
 #include("interval_parameters.jl")
 include("rationalize.jl")
-include("interval_rounding.jl")
 include("interval_parameters.jl")
+
+include("interval_rounding.jl")
+#include("interval_parameters.jl")
 include("interval_macros.jl")
 
 include("interval_conversion_promotion.jl")
+
 include("interval_arithmetic.jl")
+include("interval_precision.jl")
+
 include("interval_trig.jl")
 include("interval_functions.jl")
