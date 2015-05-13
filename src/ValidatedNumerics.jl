3b1bf2379e387eae34202295f8acdff69f6b26f2
David P. Sanders <dpsanders@gmail.com>
2015-05-13 00:43:48 -0500

    Necessary to export interval_parameters

--- src/ValidatedNumerics.jl
+++ src/ValidatedNumerics.jl
@@ -19,7 +19,8 @@ export
     diam, mid, mag, mig, hull, isinside,
     emptyinterval, ∅, isempty, ⊊,
     widen,
-    set_interval_precision, get_interval_precision
+    set_interval_precision, get_interval_precision,
+    interval_parameters
 
 ## Root finding
 export
