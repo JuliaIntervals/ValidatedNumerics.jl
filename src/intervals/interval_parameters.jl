8d91e53d49b8f59ac94c0df27f3403559b2074ce
David P. Sanders <dpsanders@gmail.com>
2015-05-14 12:05:55 -0500

    More cleanup

--- src/intervals/interval_parameters.jl
+++ src/intervals/interval_parameters.jl
@@ -1,21 +1,14 @@
-using ValidatedNumerics
-
-import ValidatedNumerics.make_interval
-
 
 type IntervalParameters
 
     precision_type::Type
     precision::Int
-
     rounding::Symbol
-
     pi::Interval{BigFloat}
 
     IntervalParameters() = new(Float64, 256, :narrow)  # leave out pi
-
-
 end
+
 
 const interval_parameters = IntervalParameters()
 
