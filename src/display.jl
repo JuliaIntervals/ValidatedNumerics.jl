5098abce513e237fa7fa71ee289fcc9bf4c5cc17
David Sanders <dpsanders@gmail.com>
2016-04-28 13:47:00 -0400

    Faster mechanism for changing precision display. Uses type-instability hack to prevent inlining

--- src/display.jl
+++ src/display.jl
@@ -8,7 +8,7 @@ const display_params = DisplayParameters(:standard, false, 6)
 
 
 
-function display_mode(;decorations=nothing, interval_display=nothing, precision::Int=-1)
+function display_mode(;decorations=nothing, interval_display=nothing, precision=-1)
     if interval_display != nothing
         display_params.interval_display = interval_display
     end
@@ -19,18 +19,31 @@ function display_mode(;decorations=nothing, interval_display=nothing, precision:
 
     if precision >= 0
         display_params.precision = precision
+        change_precision(precision)
     end
 end
 
 
-function format(x::Float64, prec::Int)
-    fmt = "%.$(prec)g"
-    @eval @sprintf($fmt, $x)
-    # hack from https://github.com/JuliaLang/julia/issues/4248
-    # Replace with Formatting.jl if supports %g
-end
 
-format(x::Float64) = format(x, display_params.precision)
+## Output
+
+function change_precision(prec::Int)
+    prec = Float64(prec)
+    prec = Int(prec)
+    #display_params.float_format = "%.$(prec)g"
+    fmt = "%.$(prec)g"
+    @eval format(x::Float64) = (@sprintf($fmt, x))
+    # creates global "format" function; add to __init__
+end
+change_precision(6)  # move to __init__
+
+
+# function format(x::Float64, prec::Int)
+#     fmt = "%.$(prec)g"
+#     @eval @sprintf($fmt, $x)
+# end
+#
+# format(x::Float64) = format(x, display_params.precision)
 
 function representation(a::Interval)
     if isempty(a)
@@ -42,10 +55,22 @@ function representation(a::Interval)
     local output
 
     if interval_display == :standard
-        aa = format(a.lo)
-        bb = format(a.hi)
+        #aa = format(a.lo)
+        #bb = format(a.hi)
 
-        output = "[$aa, $bb]"
+        # the following is a hack to make sure that format is not inlined:
+        # make it type unstable
+        aa = rand() < 2 ? a.lo : nothing
+        bb = rand() < 2 ? a.hi : nothing
+
+        aaa = format(aa)
+        bbb = format(bb)
+        #output = "[$(@noinline format(a.lo)), $(@noinline format(a.hi))]"
+        output = "[$aaa, $bbb]"
+        # prec = display_params.precision
+        # fmt = "[%.$(prec)g, %.$(prec)g]"
+        # output = @eval @sprintf($fmt, $(a.lo), $(a.hi))
+
         output = replace(output, "inf", "∞")
         output = replace(output, "Inf", "∞")
 
