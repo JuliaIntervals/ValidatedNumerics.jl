b6771256bcb3ab4fe52bacf3e04e88976c90d8f9
David Sanders <dpsanders@gmail.com>
2016-10-25 16:26:27 -0400

    Proof of concept for generic rounding macros in rounding.jl: + and sin

---
+++ src/intervals/rounding.jl
@@ -0,0 +1,63 @@
+## Optimally tight rounding by changing rounding mode:
+
+macro round_down(ex::Expr)
+    if ex.head == :call
+        op = ex.args[1]
+
+        if length(ex.args) == 3  # binary operator
+            return :( $op($(ex.args[2]), $(ex.args[3]), RoundDown) )
+
+        else  # unary operator
+            return :( $op($(ex.args[2]), RoundDown ) )
+        end
+    end
+end
+
+macro round_up(ex::Expr)
+    if ex.head == :call
+        op = ex.args[1]
+
+        if length(ex.args) == 3  # binary operator
+            return :( $op($(ex.args[2]), $(ex.args[3]), RoundUp) )
+
+        else  # unary operator
+            return :( $op($(ex.args[2]), RoundUp ) )
+        end
+    end
+end
+
+
+import Base: +, sin
+
+
+for mode in (:Down, :Up)
+
+    mode1 = Expr(:quote, mode)
+    mode1 = :(::RoundingMode{$mode1})
+
+    mode2 = Symbol("Round", mode)
+
+
+    @eval begin
+        function +{T<:AbstractFloat}(a::T, b::T, $mode1)
+            setrounding(T, $mode2) do
+                a + b
+            end
+        end
+    end
+end
+
+## Fast but not maximally tight rounding: just use prevfloat and nextfloat:
+
+        # function +{T}(a::T, b::T, ::RoundingMode{:Down})
+        #     prevfloat(a + b)
+        # end
+        #
+        # function +{T}(a::T, b::T, ::RoundingMode{:Down})
+        #     prevfloat(a + b)
+        # end
+
+
+        # function sin(a, ::RoundingMode{:Down})
+        #     prevfloat(sin(a))
+        # end
