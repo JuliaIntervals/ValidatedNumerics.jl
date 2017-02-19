f62731e981bf902fbab339fc8343037163235ebf
David Sanders <dpsanders@gmail.com>
2017-02-18 21:10:11 -0600

    Fix test

--- test/interval_tests/construction.jl
+++ test/interval_tests/construction.jl
@@ -35,10 +35,9 @@ using ValidatedNumerics
     @test Interval{Rational{Int}}(1) == Interval(1//1)
     #@test Interval{Rational{Int}}(pi) == Interval(rationalize(1.0*pi))
 
-    @test Interval{Float64}(pi) == Interval(float(pi)
+    @test Interval{Float64}(pi) == Interval(float(pi))
     @test Interval{BigFloat}(1) == Interval{BigFloat}(big(1.0), big(1.0))
-    @test Interval{BigFloat}(pi) ==
-        Interval{BigFloat}(big(pi), big(pi))
+    @test Interval{BigFloat}(pi) == Interval{BigFloat}(big(pi), big(pi))
 
     # Disallowed conversions with a > b
 
