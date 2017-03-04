633bf7a225eee66c00a1bbe48efa3547621916a0
David Sanders <dpsanders@gmail.com>
2017-03-04 07:25:43 -0600

    Fix parsing test

--- test/interval_tests/parsing.jl
+++ test/interval_tests/parsing.jl
@@ -2,6 +2,8 @@ using ValidatedNumerics
 
 setdisplay(:standard, decorations=true, sigfigs=6)
 
+setprecision(128)
+
 @testset "Parse string to Interval" begin
     @test parse(Interval{Float64}, "1") == Interval(1, 1)
     @test parse(Interval{Float64}, "[1, 2]") == Interval(1, 2)
@@ -16,7 +18,7 @@ setdisplay(:standard, decorations=true, sigfigs=6)
     @test parse(Interval{Float64}, "3 ±  4") == -1..7
     @test parse(Interval{Float64}, "0.2 ± 0.1") == Interval(0.09999999999999999, 0.30000000000000004)
     @test parse(Interval{BigFloat}, "3 ±  4") == -1..7
-    @test parse(Interval{BigFloat}, "0.2 ± 0.1") == Interval(big"1.000000000000000000000000000000000000000000000000000000000000000000000000000002e-01", big"3.000000000000000000000000000000000000000000000000000000000000000000000000000017e-01")
+    @test parse(Interval{BigFloat}, "0.2 ± 0.1") == Interval(big"1.000000000000000000000000000000000000001e-01", big"3.000000000000000000000000000000000000006e-01")
 end
 
 @testset "Parse string to DecoratedInterval" begin
