8d1c33fefffe275f911e49383e269608b924e2ee
Claudio Pierard <cl.pierard@gmail.com>
2017-03-08 09:49:50 -0600

    Improve code. Can find the big blocks

--- test/ITF1788_tests/separatebigchunks.jl
+++ test/ITF1788_tests/separatebigchunks.jl
@@ -1,4 +1,34 @@
 f = open("libieeep1788_tests_elem.jl")
-lines = readlines(f)
+code_lines = readlines(f)
 
-println(lines[15])
+A = Int[]
+
+chunk_begin = 0
+chunk_end = 0
+
+for i in 1:length(code_lines)
+
+  if length(code_lines[i]) <= 3
+
+    nothing
+
+  elseif code_lines[i][1:3] == "fac"
+
+    chunk_begin = i
+
+  elseif code_lines[i][1:3] == "end"
+
+    chunk_end = i
+
+  end
+
+  if chunk_end - chunk_begin > 100
+
+    push!(A, chunk_begin)
+    chunk_end = 0
+
+  end
+
+end
+
+println(A)
