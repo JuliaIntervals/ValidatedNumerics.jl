8952a167cf910d44f2e71e5f6417d85826e665cc
David Sanders <dpsanders@gmail.com>
2017-03-12 16:32:25 -0600

    Add docs for @format

--- src/display.jl
+++ src/display.jl
@@ -49,6 +49,34 @@ function setdisplay(format = display_params.format;
     display_params.sigfigs = sigfigs
 end
 
+doc"""
+    @format [style::Symbol] [decorations::Bool] [sigfigs::Integer]
+
+The `@format` macro provides a simple interface to control the output format
+for intervals. These options are passed to the `setdisplay` function.
+
+The arguments may be in any order and of type:
+
+- `Symbol`: the output format (`:full`, `:standard` or `:midpoint`)
+- `Bool`: whether to display decorations
+- `Integer`: the number of significant figures
+
+E.g.
+```
+julia> x = 0.1..0.3
+@[0.0999999, 0.300001]
+
+julia> @format full
+
+julia> x
+Interval(0.09999999999999999, 0.30000000000000004)
+
+julia> @format standard 3
+
+julia> x
+[0.0999, 0.301]
+```
+"""
 macro format(expr...)
     for ex in expr
         if isa(ex, Symbol)
