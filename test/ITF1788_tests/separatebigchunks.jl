2c55b3bf98bf8c37038b21c02d3a198eb39a512f
Claudio Pierard <cl.pierard@gmail.com>
2017-03-13 22:43:24 -0600

    cambio de rama para reescribir código

--- test/ITF1788_tests/separatebigchunks.jl
+++ test/ITF1788_tests/separatebigchunks.jl
@@ -1,22 +1,18 @@
 f = open("libieeep1788_tests_elem.jl")
 code_lines = readlines(f)
 
-A = Int[]
+A = []
 
 chunk_begin = 0
 chunk_end = 0
 
 for i in 1:length(code_lines)
 
-  if length(code_lines[i]) <= 3
-
-    nothing
-
-  elseif code_lines[i][1:3] == "fac"
+  if startswith(code_lines[i], "facts")
 
     chunk_begin = i
 
-  elseif code_lines[i][1:3] == "end"
+  elseif startswith(code_lines[i], "end")
 
     chunk_end = i
 
@@ -24,10 +20,24 @@ for i in 1:length(code_lines)
 
   if chunk_end - chunk_begin > 100
 
-    push!(A, chunk_begin)
+    push!(A, [chunk_begin, chunk_end])
     chunk_end = 0
 
   end
+
+end
+
+#for i in length(A)
+
+  insert!(code_lines, A[1][1], "BEGIN")
+  #insert!(code_lines, A[i][1] + 50, "")
+  #insert!(code_lines, A[i][1] + 50, "end")
+
+#end
+
+for j in A[1][1]-1:A[1][1]+1
+
+  println(code_lines[j])
 
 end
 
