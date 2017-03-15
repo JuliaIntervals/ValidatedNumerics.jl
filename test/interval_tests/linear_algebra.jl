2e499f7c18de8d477b8f447fc6adf9bb79e1bb52
David Sanders <dpsanders@gmail.com>
2017-03-15 17:21:03 -0600

    Reenable linear algebra test

--- test/interval_tests/linear_algebra.jl
+++ test/interval_tests/linear_algebra.jl
@@ -17,12 +17,7 @@ b = [-2..2
 
     # Example from Moore et al., Introduction to Interval Analysis (2009), pg. 88:
 
-    if VERSION <= v"0.5"
     @test A \ b == [-5..5
                     -4..4]
-    else
-        @test_broken A \ b == [-5..5
-                               -4..4]
-                    end
 
 end
