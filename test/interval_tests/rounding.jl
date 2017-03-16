c02caed87a9dbe489ed0af86dfe8ff2ea3f32d06
David Sanders <dpsanders@gmail.com>
2017-03-15 19:43:05 -0600

    Correct testset syntax

--- test/interval_tests/rounding.jl
+++ test/interval_tests/rounding.jl
@@ -1,7 +1,8 @@
-
+using ValidatedNumerics
 using Base.Test
 
-@testset("Interval rounding") begin
+
+@testset "Interval rounding" begin
 
     x = Interval(0.5)
     @test sin(x) == Interval(0.47942553860420295, 0.479425538604203)
