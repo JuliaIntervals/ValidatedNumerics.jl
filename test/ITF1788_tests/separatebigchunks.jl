1ee4ae7d6058059fb342998d5109a6fe2a4b6950
Claudio Pierard <cl.pierard@gmail.com>
2017-03-18 00:20:57 -0600

    Wrap separatebigchunks.jl code in a function

--- test/ITF1788_tests/separatebigchunks.jl
+++ test/ITF1788_tests/separatebigchunks.jl
@@ -1,77 +1,85 @@
+```
+  separatechunks(file_name ::String, size_criterium::Int, new_size::Int)
 
-f = open("libieeep1788_tests_elem.jl")
-code_lines = readlines(f) #Creates array with all the code lines
-code_lines_new = [] #The new array
 
-n_bs = 30 #new blocks size
+```
 
-aux_index = 1 #number that helps appending the 'good' blocks
-chunk_begin = 0 #indicates where the big chunk begins
-chunk_end = 0 #where the big chunk end
+function separatechunks(file_name ::String, size_criterium::Int, new_size::Int)
+  
+  f = open("libieeep1788_tests_elem.jl")
+  code_lines = readlines(f) #Creates array with all the code lines
+  code_lines_new = [] #The new array
 
-for i in 1:length(code_lines)
+  #new_size = 30 #new blocks size
 
-  if startswith(code_lines[i], "facts")
+  aux_index = 1 #number that helps appending the 'good' blocks
+  chunk_begin = 0 #indicates where the big chunk begins
+  chunk_end = 0 #where the big chunk end
 
-    chunk_begin = i
+  for i in 1:length(code_lines)
 
-  elseif startswith(code_lines[i], "end")
+    if startswith(code_lines[i], "facts")
 
-    chunk_end = i
+      chunk_begin = i
+
+    elseif startswith(code_lines[i], "end")
+
+      chunk_end = i
+
+    end
+
+    if chunk_end - chunk_begin > size_criterium #detecting big blocks of code
+
+      chunk_length = chunk_end - chunk_begin
+
+      #Appending the blocks that have the good size
+
+      append!(code_lines_new, code_lines[aux_index:chunk_begin - 1])
+
+      ## Extract block name: facts("block_name") do
+
+      b = search(code_lines[chunk_begin], "\") do")[1] - 1
+      a = search(code_lines[chunk_begin], "facts(\"")[end] +1
+      title = code_lines[chunk_begin][a:b]
+      number = 1
+
+      # Divide big chunks in smaller blocks
+
+      residue = chunk_length%new_size
+
+        for j in 0:floor(Int, chunk_length / new_size) - 1
+
+          push!(code_lines_new, "facts(\"$title" * "_" * "$number\") do", "\n")
+
+          append!(code_lines_new, code_lines[chunk_begin + 1 + j*new_size  : chunk_begin + (j + 1)*new_size])
+
+          push!(code_lines_new, "end", "\n", "\n")
+
+          number += 1
+
+        end
+
+        if residue > 0
+
+          push!(code_lines_new, "facts(\"$title" * "_" * "$number\") do", "\n")
+
+          append!(code_lines_new, code_lines[chunk_end - residue + 1 : chunk_end - 1])
+
+          push!(code_lines_new, "end", "\n")
+
+        end
+
+      aux_index = chunk_end + 1
+      chunk_end = 0
+
+    end
 
   end
 
-  if chunk_end - chunk_begin > 100 #detecting big blocks of code
+  #writing the new file
 
-    chunk_length = chunk_end - chunk_begin
-
-    #Appending the blocks that have the good size
-
-    append!(code_lines_new, code_lines[aux_index:chunk_begin - 1])
-
-    ## Extract block name: facts("block_name") do
-
-    b = search(code_lines[chunk_begin], "\") do")[1] - 1
-    a = search(code_lines[chunk_begin], "facts(\"")[end] +1
-    title = code_lines[chunk_begin][a:b]
-    number = 1
-
-    # Divide big chunks in smaller blocks
-
-    residue = chunk_length%n_bs
-
-      for j in 0:floor(Int, chunk_length / n_bs) - 1
-
-        push!(code_lines_new, "facts(\"$title" * "_" * "$number\") do", "\n")
-
-        append!(code_lines_new, code_lines[chunk_begin + 1 + j*n_bs  : chunk_begin + (j + 1)*n_bs])
-
-        push!(code_lines_new, "end", "\n", "\n")
-
-        number += 1
-
-      end
-
-      if residue > 0
-
-        push!(code_lines_new, "facts(\"$title" * "_" * "$number\") do", "\n")
-
-        append!(code_lines_new, code_lines[chunk_end - residue + 1 : chunk_end - 1])
-
-        push!(code_lines_new, "end", "\n")
-
-      end
-
-    aux_index = chunk_end + 1
-    chunk_end = 0
-
-  end
-
+  new_file = join(code_lines_new)
+  f_new = open("test_file.jl", "w")
+  print(f_new, new_file)
+  close(f_new)
 end
-
-#writing the new file
-
-new_file = join(code_lines_new)
-f_new = open("test_file.jl", "w")
-print(f_new, new_file)
-close(f_new)
