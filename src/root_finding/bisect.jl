126e444e3178e623bcacaf8810b0680e0e25e77f
David Sanders <dpsanders@gmail.com>
2017-03-19 08:12:11 -0600

    Add geometric midpoints for bisect

--- src/root_finding/bisect.jl
+++ src/root_finding/bisect.jl
@@ -1,14 +1,38 @@
 
 doc"""
+    geometric_mid(X::Interval)
+
+Find something like a geometric midpoint.
+"""
+function geometric_mid(X::Interval)
+
+    m = sqrt(max(abs(X.lo), abs(X.hi)))
+
+    if m ∉ X
+        m = -m  # fix sign
+    end
+
+    return m
+end
+
+doc"""
     bisect(X::Interval, α=0.5)
 
 Split the interval `X` at position α; α=0.5 corresponds to the midpoint.
+If the interval has a large dynamic range, splits in a geometric, rather than arithmetic, way.
+
 Returns a tuple of the new intervals.
 """
 function bisect(X::Interval, α=0.5)
     @assert 0 ≤ α ≤ 1
 
-    m = mid(X, α)
+    dynamic_range = abs(X.lo) / abs(X.hi)
+
+    if isinf(X) || 1e-2 < dynamic_range < 1e2
+        m = mid(X, α)  # use normal midpoint
+    else
+        m = geometric_mid(X)
+    end
 
     return (Interval(X.lo, m), Interval(m, X.hi))
 end
