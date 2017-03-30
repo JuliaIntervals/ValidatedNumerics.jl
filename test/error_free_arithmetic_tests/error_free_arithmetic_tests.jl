b1dc8897b14cba6d7029d443a309baa892cc750c
David Sanders <dpsanders@gmail.com>
2017-03-30 19:50:18 -0400

    Add random errorfree tests

--- test/error_free_arithmetic_tests/error_free_arithmetic_tests.jl
+++ test/error_free_arithmetic_tests/error_free_arithmetic_tests.jl
@@ -4,6 +4,8 @@ using Base.Test
 
 const errorfree = ValidatedNumerics.IntervalRounding{:errorfree}
 
+srand(17)  # for reproducible random numbers
+
 @testset "Errorfree arithmetic tests" begin
 
     @testset "+, -, *, /" begin
@@ -11,12 +13,15 @@ const errorfree = ValidatedNumerics.IntervalRounding{:errorfree}
         for s1 in (+, -)  # sign
             for s2 in (+, -)  # sign
 
-                a, b = s1(0.1), s2(0.7)
+                for i in 1:1000
+                    a, b = rand(0.0:0.1:100000, 2)
+                    a, b = s1(a), s2(b)  # signs
 
-                for r in (RoundUp, RoundDown)
-                    for op in (+, -, *, /)
-                        # @show op, a, b, r
-                        @test op(a, b, r) == op(errorfree, a, b, r)
+                    for r in (RoundUp, RoundDown)
+                        for op in (+, -, *, /)
+                            # @show op, a, b, r
+                            @test op(a, b, r) == op(errorfree, a, b, r)
+                        end
 
                     end
                 end
@@ -26,8 +31,8 @@ const errorfree = ValidatedNumerics.IntervalRounding{:errorfree}
 
     @testset "sqrt" begin
         for r in (RoundUp, RoundDown)
-            for a in (10.0, 0.3, 1024.5, 49.0)
-                @show a, r 
+            for a in [10.0, 0.3, 1024.5, 49.0] ∪ rand(0.0:0.001:1, 1000) ∪  rand(1.:0.01:1000, 1000)
+                # @show a, r
                 @test sqrt(errorfree, a, r) == sqrt(a, r)
             end
         end
