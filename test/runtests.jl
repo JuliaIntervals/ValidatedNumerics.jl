8f36c6232087e2a3a0740a88927a802bae66c0d4
David Sanders <dpsanders@gmail.com>
2018-10-16 15:02:08 -0500

    Updates for Julia 1.0

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,11 @@
 using ValidatedNumerics
 
-using Base.Test
+using Test
 
 @testset "Load and briefly test packages" begin
+
     @testset "IntervalArithmetic" begin
-        @test @interval(1, 2) == Interval(1, 2)
+        @test 1..2 == Interval(1, 2)
     end
 
     @testset "IntervalRootFinding" begin
@@ -17,7 +18,30 @@ using Base.Test
         X = (-∞..∞) × (-∞..∞)
         paving = pave(S, X, 1.0)
 
-        @test length(paving.inner) == 4
-        @test length(paving.boundary) == 8
+        @test length(paving.inner) == 3
+        @test length(paving.boundary) == 7
     end
+
+    @testset "IntervalContractors" begin
+        X = 1..4
+        Y = 5..10
+
+        Z = X + Y
+
+        Z = Z ∩ (13..15)
+
+        Z, X, Y = plus_rev(Z, X, Y)
+
+        @test X == 3..4
+        @test Y == 9..10
+    end
+
+    @testset "IntervalOptimisation" begin
+        f(x) = x^4 - 3x^3 + 2x
+
+        globalmin, minimisers = minimise(f, -1e10..1e10, 1e-7)
+
+        @test globalmin ⊆ (-4.15.. -4.14)
+    end
+
 end
